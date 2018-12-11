5d1e1b8c4f1a592ed8263b39e39a989d7b9168f2
Chad Ridings <chadridings@gmail.com>
2018-12-10 23:13:34 -0800

    [Master] Explained how an event bus would be used

--- src/events.js
+++ src/events.js
@@ -4,15 +4,15 @@ const EventBus = new Vue();
 export default EventBus;
 
 /*
+    Not used in this project, but this is how you would pass events from one component to another.
+    Think of it as a bus (like a metro) that picks up events from one location and drops them off at another.
     Event Bus Example Use:
-
 
     In the file you wish to emit the event from...
 
     import EventBus from '../events';
     EventBus.$emit('emitEvent', data.product.id);
 
-    
 
     In the file you wish to capture the event from...  
 
