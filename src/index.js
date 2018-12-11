2410d31ebd0182209a35e51aef121e88b30032bc
Chad Ridings <chadridings@gmail.com>
2018-12-10 23:52:40 -0800

    [Master] update package file, package-lock must be included due to dependencies.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
 import Vue from 'vue';
-import VueDragDrop from 'vue-drag-drop';
 import MainContent from './views/main-content';
-
-Vue.use(VueDragDrop);
 
 let MainComponent = Vue.extend(MainContent);
 new MainComponent().$mount("#app");
